16b8bc57019dcd89a4d2e67bd77f1a19b9d18f10
EddiesLife247 <eddieslife247@pm.me>
2023-03-13 13:13:52 +0000

    Revert "Update API disconnect feature as was leaving aircraft connected after simulator closed."
    This reverts commit e490c0618c1600cd06f009db9a6ed2467bd27c12.

--- JoinFS DotNet/JoinFsMain.vb
+++ JoinFS DotNet/JoinFsMain.vb
@@ -80,7 +80,7 @@ Public Class JoinFsMain
         Process.Start("explorer.exe", MainVariables.supportLink)
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+    Private Async Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         If (Button3.BackColor = Color.Red) Then
             If p3d_simconnect Is Nothing Then
                 If (My.Settings.Simulator = "P3Dv5" Or My.Settings.Simulator = "MSFS" Or My.Settings.Simulator = "") Then
@@ -122,6 +122,7 @@ Public Class JoinFsMain
             End If
         Else
             AddLogItem("Disconnected from simulator")
+            Await API("Stop")
             ConnectedText.Text = "Simulator Disconnected"
             SimTimer.Enabled = False
             closeConnection()
